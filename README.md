3a0228d4f64cfdc5804a7b71ed4a1de93964dd41
Thomas Fankhauser <tommy@system8.io>
2018-01-18 23:39:05 +0100

    Refactored and updated readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# st8
+
+Local static server for development, file view, etc. written in Go
+
+## Install
+
+Install using `go install` and maybe create a systemd unit for it
--- main.go
+++ main.go
@@ -13,42 +13,40 @@ func main() {
 	host := flag.String("host", "home", "Hostname")
 	flag.Parse()
 
-	http.Handle("/", NoCache(http.FileServer(http.Dir(*dir))))
+	http.Handle("/", noCache(http.FileServer(http.Dir(*dir))))
 
 	log.Printf("Serving %s on %s:%s\n", *dir, *host, *port)
 	log.Fatal(http.ListenAndServe(*host+":"+*port, nil))
 }
 
-var epoch = time.Unix(0, 0).Format(time.RFC1123)
+// wraps a handler to disable caching
+func noCache(h http.Handler) http.Handler {
+	var headersToSet = map[string]string{
+		"Expires":         time.Unix(0, 0).Format(time.RFC1123),
+		"Cache-Control":   "no-cache, private, max-age=0",
+		"Pragma":          "no-cache",
+		"X-Accel-Expires": "0",
+	}
 
-var noCacheHeaders = map[string]string{
-	"Expires":         epoch,
-	"Cache-Control":   "no-cache, private, max-age=0",
-	"Pragma":          "no-cache",
-	"X-Accel-Expires": "0",
-}
+	var headersToDelete = []string{
+		"ETag",
+		"If-Modified-Since",
+		"If-Match",
+		"If-None-Match",
+		"If-Range",
+		"If-Unmodified-Since",
+	}
 
-var etagHeaders = []string{
-	"ETag",
-	"If-Modified-Since",
-	"If-Match",
-	"If-None-Match",
-	"If-Range",
-	"If-Unmodified-Since",
-}
-
-func NoCache(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		// delete etags
-		for _, v := range etagHeaders {
+		// delete
+		for _, v := range headersToDelete {
 			if r.Header.Get(v) != "" {
 				r.Header.Del(v)
 			}
 		}
 
-		// set no cache headers
-		for k, v := range noCacheHeaders {
+		// set
+		for k, v := range headersToSet {
 			w.Header().Set(k, v)
 		}
 
